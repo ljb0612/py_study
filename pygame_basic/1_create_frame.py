22c103dc5da21e90cd426ce6a15456a7b48b1d2a
ljb0612 <ljb0612@gmail.com>
2022-07-16 01:20:39 +0900

    game_start

---
+++ pygame_basic/1_create_frame.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()   # 초기화 (무조건 필요)
+
+# 화면 크기 설정
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width,screen_height))
+
+# 화면 title
+pygame.display.set_caption("Game Test")
