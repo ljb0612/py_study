c7c4a7606800bcb04a1c4448c49a34116a730513
ljb0612 <ljb0612@gmail.com>
2022-07-16 01:25:02 +0900

    game_start_end

--- pygame_basic/1_create_frame.py
+++ pygame_basic/1_create_frame.py
@@ -9,3 +9,13 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 
 # 화면 title
 pygame.display.set_caption("Game Test")
+
+# event loop
+running = True
+while running:
+    for event in pygame.event.get():    # 무조건 필요 (마우스,키보드 이벤트 체크)
+        if event.type == pygame.QUIT:
+            running = False
+
+# pygame 종료
+pygame.quit()
