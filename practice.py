d85bf616d22f3b36243a0e847f3818d958b5e547
ljb0612 <ljb0612@gmail.com>
2022-07-16 01:07:09 +0900

    module

--- practice.py
+++ practice.py
@@ -1044,9 +1044,119 @@ import pickle
 
 
 #------------------------------------------#
-# Module - 부붐처럼 잘 만들어진 파일
+# Module - 부품처럼 잘 만들어진 파일
 #------------------------------------------#
-# 1
+# # 1
+# import theater_module
+# theater_module.price(3)
+# theater_module.price_morning(4)
+# theater_module.price_army(5)
+
+# # 2
+# import theater_module as mv
+# mv.price(3)
+# mv.price_morning(4)
+# mv.price_army(5)
+
+# # 3
+# from theater_module import *
+# price(3)
+# price_morning(4)
+# price_army(5)
+
+# # 4
+# from theater_module import price, price_morning
+# price(3)
+# price_morning(4)
+
+# # 5
+# from theater_module import price_army as price
+# price(3)
+
+# # 6 - package
+# # import는 모듈이나 패키지만 가능
+# import travel.thailand
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
+
+# # 7
+# from travel.thailand import ThailandPackage
+# trip_to = ThailandPackage()
+# trip_to.detail()
+
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
+
+# # 8 - __all__
+# from travel import *
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
+
+# trip_to2 = thailand.ThailandPackage()
+# trip_to2.detail()
+
+# # 9 - 위치 확인 - lib 폴더 아래에 두면 어느 프로젝트던 사용 가능
+# from travel import *
+# import inspect
+# import random
+# print(inspect.getfile(random))
+# print(inspect.getfile(thailand))
+
+# # 10 - pip install (pypi search)
+# from bs4 import BeautifulSoup
+# soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
+# print(soup.prettify())
+
+# 11 - 내장 함수
+# lan = input("무슨 언어를 좋아해?")
+# print("{0}은 아주 좋은 언어야".format(lan))
+
+#dir - 어떤 객체를 넘겨줬을 때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
+# print(dir())
+# import random
+# print(dir(random))
+
+# lst = [1,2,3]
+# print(dir(lst))
+
+# name = "aaa"
+# print(dir(name))
+
+# google search - list of python builtins (내장함수 정보)
+
+# 12 - 외장 함수 (직접 import) (list of python modules)
+# # - glob : 경로 내의 폴더 / 파일 목록 조회 (윈도우 dir)
+# import glob
+# print(glob.glob("*.py"))
+
+# os : 운영체제에서 제공하는 기본 기능
+# import os
+# print(os.getcwd())
+
+# folder = "sample_dir"
+
+# if os.path.exists(folder):
+#     print("이미 존재")
+#     os.rmdir(folder)
+#     print(folder,"폴더를 삭제하였습니다.")
+# else:
+#     os.makedirs(folder)
+#     print(folder, "폴더를 생성하였습니다")
+
+# print(os.listdir())
+
+# time : 시간 관련 함수
+# import time
+# print(time.localtime())
+# print(time.strftime("%Y-%m-%d %H:%M:%S"))
+import datetime
+print("오늘 날짜는 ", datetime.date.today())
+
+# timedelta : 두 날짜 사이의 간격
+today = datetime.date.today()
+td = datetime.timedelta(100)
+print(today + td)
 
 #------------------------------------------#
 #------------------------------------------#
