8138130732bcc432207d24ac50603647045a6498
ljb0612 <ljb0612@gmail.com>
2022-07-27 23:15:11 +0900

    btn

--- gui_basic/1_create_frame.py
+++ gui_basic/1_create_frame.py
@@ -1,5 +1,11 @@
+from math import fabs
 from tkinter import *
 
 root = Tk()
 root.title("test")
+root.geometry("640x480")
+#root.geometry("640x480+100+300") # 가로 세로 x좌표 y좌표
+
+root.resizable(False,False)
+
 root.mainloop()
